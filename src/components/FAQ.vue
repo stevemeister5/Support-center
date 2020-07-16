192e923040165d70e03eb3806bf4adfc4b6af91b
stevemeister5 <stephenreubenm@gmail.com>
2020-07-16 12:25:05 +0300

    Navigation Setup

---
+++ src/components/FAQ.vue
@@ -0,0 +1,5 @@
+<template>
+    <main class="faq">
+        <h1>Frequently asked Questions</h1>
+    </main>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import 'babel-polyfill'
 import Vue from 'vue'
+import router from './router'
+
+import AppLayout from './components/AppLayout.vue'
 
 new Vue({
     el: '#app',
-    render: h => h('div', 'Support center'),
+    render: h => h(AppLayout),
+    // Provide the router to the app
+    router,
 })
