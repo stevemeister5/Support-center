8d72c66af016acc452c5a13f752141d9d852d0cb
stevemeister5 <stephenreubenm@gmail.com>
2020-08-02 19:30:08 +0300

    FormComponent Setup

--- src/global-components.js
+++ src/global-components.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
 import Loading from './components/Loading.vue'
+import SmartForm from './components/SmartForm.vue'
+import FormInput from './components/FormInput.vue'
 
+Vue.component('FormInput', FormInput)
 Vue.component('Loading', Loading)
+Vue.component('SmartForm', SmartForm)
--- src/router.js
+++ src/router.js
@@ -1,21 +1,22 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import Home from './components/Home.vue'
-import FAQ from './components/FAQ.vue'
+import Home from "./components/Home.vue";
+import FAQ from "./components/FAQ.vue";
+import Login from "./components/Login.vue";
 
-
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    // Routes will be here
-    { path: '/', name: 'home', component: Home },
-    { path: '/faq', name: 'faq', component: FAQ },
-]
+  // Routes will be here
+  { path: "/", name: "home", component: Home },
+  { path: "/faq", name: "faq", component: FAQ },
+  { path: "/login", name: "login", component: Login }
+];
 
 const router = new VueRouter({
-    routes,
-    mode: 'history',
-})
+  routes,
+  mode: "history"
+});
 
-export default router
+export default router;
